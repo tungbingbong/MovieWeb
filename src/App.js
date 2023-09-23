df6b510f4cc7fc14fe278b5b06d7e44fda45fb6f
tungbingbong <hung2522001@gmail.com>
2023-09-23 23:02:40 +0700

    adding redux #2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { Fragment, lazy, Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+/* eslint-disable react-hooks/exhaustive-deps */
 import 'swiper/scss';
+import axios from 'axios';
+import { Fragment, lazy, Suspense, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { useSelector, useDispatch } from 'react-redux';
 
 import './App.css';
+import { tmdb } from './config';
 import Main from '~/components/layout/Main';
-import ProfilePageMain from './components/layout/ProfilePageMain';
+import { auth, db } from './firebase-config';
 import Banner from './components/banner/Banner';
+import { setUserInfo } from './redux/AuthSlice/authSlice';
+import ProfilePageMain from './components/layout/ProfilePageMain';
+import {
+    setBookmarkId,
+    setCurrentId,
+    setHistory,
+    setMoviesBookmarkData,
+    setMoviesHistory,
+} from './redux/PersonalSlice/personalSlice';
 
 import NotFoundPage from './pages/NotFoundPage';
 import MovieSearchPage from './pages/Movies/MovieSearchPage';
@@ -20,7 +35,60 @@ const MoviePage = lazy(() => import('~/pages/Movies/MoviePage'));
 const MovieDetailPage = lazy(() => import('~/pages/Movies/MovieDetailPage'));
 const GenresSearchPage = lazy(() => import('~/pages/GenresSearchPage'));
 
+// https://api.themoviedb.org/3/movie/now_playing?api_key=68ff44b16c8cfc514f5219295b422d75&language=en-US&page=1
+
 function App() {
+    const dispatch = useDispatch();
+    const { history, bookmarkId } = useSelector((state) => state.personal);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                dispatch(setUserInfo(currentUser));
+            } else {
+                dispatch(setUserInfo(''));
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+        const arr = [];
+        const bookmarkArr = [];
+
+        history.forEach((item) => {
+            axios.get(tmdb.getMovieDetails(item, null)).then((res) => {
+                arr.push(res.data);
+                dispatch(setMoviesHistory([...arr]));
+            });
+        });
+
+        bookmarkId.forEach((item) => {
+            axios.get(tmdb.getMovieDetails(item, null)).then((res) => {
+                bookmarkArr.push(res.data);
+                dispatch(setMoviesBookmarkData([...bookmarkArr]));
+            });
+        });
+    }, [history, bookmarkId]);
+
+    const userList = collection(db, 'users');
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                const uid = currentUser.uid;
+                onSnapshot(userList, (snapshot) => {
+                    snapshot.forEach((doc) => {
+                        if (doc.data().uid === uid) {
+                            dispatch(setHistory(JSON.parse(doc.data().history)));
+                            dispatch(setBookmarkId(JSON.parse(doc.data().bookmark)));
+                            dispatch(setCurrentId(doc.id));
+                        }
+                    });
+                });
+            }
+        });
+    }, []);
+
     return (
         <Fragment>
             <Suspense>
